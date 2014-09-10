62482b95cd9648638b91a2c6d218375972b84c49
Geir Sagberg <geir.sagberg@gmail.com>
2014-09-10 10:53:32 +0200

    Absolute path i bookmarklet

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,2 +1,2 @@
 var content = document.getElementById('content');
-content.innerHTML='<object type="text/html" data="app.html" style="width: 600px; height: 600px;"></object>';
+content.innerHTML='<object type="text/html" data="http://telenor-dekning.herokuapp.com/app.html" style="width: 600px; height: 600px;"></object>';
