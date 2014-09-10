e9c99e1eb8ec6fbf563ebded72cb32c827471015
Geir Sagberg <geir.sagberg@gmail.com>
2014-09-10 10:59:18 +0200

    Set width and height for embedded object

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,2 +1,3 @@
 var content = document.getElementById('content');
-content.innerHTML='<object type="text/html" data="http://telenor-dekning.herokuapp.com/app.html" style="width: 600px; height: 600px;"></object>';
+//content.setAttribute("style", "width: 100%; height: 100%;");
+content.innerHTML='<object type="text/html" data="http://telenor-dekning.herokuapp.com/app.html" style="width: 100%; height: 100%;"></object>';
