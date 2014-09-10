3ee48cb04b2ac9f9356123327752d02c8578cb06
Geir Sagberg <geir.sagberg@gmail.com>
2014-09-10 15:23:06 +0200

    Fikset bootstrap.js

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,13 +1,13 @@
 var content = document.getElementById('content');
 //content.setAttribute("style", "width: 100%; height: 100%;");
-content.innerHTML='<!--[if IE]>
-<object classid="clsid:25336920-03F9-11CF-8FD0-00AA00686F13" data="http://telenor-dekning.herokuapp.com/app.html">
-<p>Dekningskartet fungerer kun med nyere nettlesere. Vennligst oppgrader nettleseren din.</p>
-</object>
-<![endif]-->
-
-<!--[if !IE]> <-->
-<object type="text/html" data="http://telenor-dekning.herokuapp.com/app.html">
-<p>Dekningskartet fungerer kun med nyere nettlesere. Vennligst oppgrader nettleseren din.</p>
-</object>
+content.innerHTML='<!--[if IE]> \
+<object classid="clsid:25336920-03F9-11CF-8FD0-00AA00686F13" data="http://telenor-dekning.herokuapp.com/app.html"> \
+<p>Dekningskartet fungerer kun med nyere nettlesere. Vennligst oppgrader nettleseren din.</p> \
+</object> \
+<![endif]--> \
+\
+<!--[if !IE]> <--> \
+<object type="text/html" data="http://telenor-dekning.herokuapp.com/app.html"> \
+<p>Dekningskartet fungerer kun med nyere nettlesere. Vennligst oppgrader nettleseren din.</p> \
+</object> \
 <!--> <![endif]-->';
