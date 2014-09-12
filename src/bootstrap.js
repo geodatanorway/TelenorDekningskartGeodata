60c69edf73a1b1de68eb86f79cd16ab35c9282e9
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2014-09-12 12:21:36 +0200

    Fjerner gamle ie-hacks. Legger på random query-parameter på app.html så den ikke caches.

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,13 +1,3 @@
+var random = Math.random();
 var content = document.getElementById('content');
-//content.setAttribute("style", "width: 100%; height: 100%;");
-content.innerHTML='<!--[if IE]> \
-<object classid="clsid:25336920-03F9-11CF-8FD0-00AA00686F13" data="http://telenor-dekning.herokuapp.com/app.html" style="width: 100%; height: 100%"> \
-<p>Dekningskartet fungerer kun med nyere nettlesere. Vennligst oppgrader nettleseren din.</p> \
-</object> \
-<![endif]--> \
-\
-<!--[if !IE]> <--> \
-<object type="text/html" data="http://telenor-dekning.herokuapp.com/app.html" style="width: 100%; height: 100%"> \
-<p>Dekningskartet fungerer kun med nyere nettlesere. Vennligst oppgrader nettleseren din.</p> \
-</object> \
-<!--> <![endif]-->';
+content.innerHTML = '<object type="text/html" data="http://telenor-dekning.herokuapp.com/app.html?r=' + random + '" style="width: 100%; height: 100%"><p>Dekningskartet fungerer kun med nyere nettlesere. Vennligst oppgrader nettleseren din.</p></object>';
