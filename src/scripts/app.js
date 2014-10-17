a500c1a48c886c6df32bee4ae9f52a833d7ba1e7
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2014-10-17 12:06:29 +0200

    Endringer fra ludvig: Endre placeholder tekst på mobil.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,9 +2,11 @@ var ko = require('knockout');
 var $ = require('zepto-browserify').$;
 var NProgress = require('nprogress');
 var toggler = require('./toggler');
+var _ = require('lodash');
 
 var map = require('./map');
 var ViewModel = require('./ViewModel');
+var matchMedia = require('./libs/match-media');
 
 // removes the ~200ms delay on mobile
 var attachFastClick = require('fastclick');
@@ -24,3 +26,14 @@ $(document).on("click", e => {
 toggler('.toggler');
 
 ko.applyBindings(viewModel);
+
+
+function changePlaceholderOnSearchInput () {
+  var isMobile = matchMedia('only screen and (max-width: 480px)').matches;
+  if (isMobile) {
+    $('.search-input').attr('placeholder', 'Søk');
+  }
+}
+
+changePlaceholderOnSearchInput();
+$(window).resize(_.debounce(changePlaceholderOnSearchInput, 200));
